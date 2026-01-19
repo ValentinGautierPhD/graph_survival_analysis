bfb1cac9553a0e7ec7077a6538b34e48dad700d6
Valentin <valentin.gautier.inp@gmail.com>
2026-01-19 14:36:01 +0100

    Honestly i don't even remember

--- src/survival_analysis/experiments/train_survival.py
+++ src/survival_analysis/experiments/train_survival.py
@@ -1,84 +1,17 @@
-from typing import Any, Dict, List, Optional, Tuple
 
-from lightning.pytorch import LightningModule, Trainer, Callback, LightningDataModule
-from lightning.pytorch.loggers import Logger
-import torch
+from typing import Optional
 import hydra
 from omegaconf import DictConfig
 
 from ..utils import (
     RankedLogger,
-    extras,
-    get_metric_value,
-    instantiate_callbacks,
-    instantiate_loggers,
-    log_hyperparameters,
-    task_wrapper,
 )
-
-import torch
+from ..utils.train_test import train
 
 log = RankedLogger(__name__, rank_zero_only=True)
 
 
-@task_wrapper
-def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
-    training.
-
-    This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
-    failure. Useful for multiruns, saving info about the crash, etc.
-
-    :param cfg: A DictConfig configuration composed by Hydra.
-    :return: A tuple with metrics and dict with all instantiated objects.
-    """
-    # set seed for random number generators in pytorch, numpy and python.random
-    # if cfg.get("seed"):
-    #     L.seed_everything(cfg.seed, workers=True)
-
-    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
-    datamodule.setup('fit') #allows for input / output features to be configured in the model
-
-    log.info(f"Instantiating model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model, in_features=datamodule.in_dims, out_features=datamodule.out_dims)
-
-    log.info("Instantiating callbacks...")
-    callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
-
-    log.info("Instantiating loggers...")
-    logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
-
-    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
-
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "callbacks": callbacks,
-        "logger": logger,
-        "trainer": trainer,
-    }
-
-    if logger:
-        log.info("Logging hyperparameters!")
-        log_hyperparameters(object_dict)
-
-    if cfg.get("train"):
-        log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
-
-    train_metrics = trainer.callback_metrics
-
-    # merge train and test metrics
-    metric_dict = {**train_metrics}
-
-    
-    return metric_dict, object_dict
-
-
-@hydra.main(version_base="1.3", config_path="../../../configs", config_name="train_survival.yaml")
+@hydra.main(version_base="1.3", config_path="../../../configs", config_name="train_graphs.yaml")
 def main(cfg: DictConfig) -> Optional[float]:
     """Main entry point for training.
 
@@ -89,7 +22,7 @@ def main(cfg: DictConfig) -> Optional[float]:
     # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
     # extras(cfg)
 
-    train(cfg)
+    train(cfg, log)
     
 if __name__ == "__main__":
     main()
